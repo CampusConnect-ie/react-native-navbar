730db1594f1b5b7563acfb5e00d595cefca5e572
Burhan Shakir <burhanshakir96@gmail.com>
2019-09-21 17:28:36 +0100

    Update NavBar.js
    Importing prop-types

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,5 +1,5 @@
-import React, { Component, PropTypes } from 'react'
-import { Navigator } from 'react-native-deprecated-custom-components'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'import { Navigator } from 'react-native-deprecated-custom-components'
 import { isSameUrl, findRoute } from './routing'
 import routeMapper from './route-mapper'
 
